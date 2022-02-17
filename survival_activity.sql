ae2e3ada2cd5abd77875432725c796ad9d92d7fe
thisisvkt <99861988+thisisvkt@users.noreply.github.com>
2022-02-17 11:50:38 +0530

    Add files via upload

---
+++ survival_activity.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 17, 2022 at 07:10 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `edynamic_edynamic`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `survival_activity`
+--
+
+CREATE TABLE `survival_activity` (
+  `sr_no` int(10) NOT NULL,
+  `activity` varchar(100) NOT NULL,
+  `earn` int(10) NOT NULL,
+  `loose` int(10) NOT NULL,
+  `phase` int(1) NOT NULL,
+  `user_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ table1.php
@@ -0,0 +1,37 @@
+<table class="table-business">
+	
+  <tr>
+    <th>SR. NO.</th>
+    <th>SURVIVAL ACTIVITIES</th>
+    <th>INCOME YOU EARN</th>
+	<th>INCOME YOU LOOSE COMPARE TO YOUR BENCHMARK</th>
+	<th>ACTIVITIES YOU NEED DO IN PHASE 2,3,4,5,6</th>
+  </tr>
+  <tr>
+    <td contenteditable='true' id="sr_no:1" ></td>
+	<td contenteditable='true' id="activity:1" ></td>
+	<td contenteditable='true' id="earn:1" ></td>
+	<td contenteditable='true' id="loose:1"></td>
+	<td contenteditable='true' id="phase:1"></td>
+  </tr>
+  <tr>
+    <td contenteditable='true' id="sr_no:2"></td>
+	<td contenteditable='true' id="activity:2"></td>
+	<td contenteditable='true' id="earn:2"></td>
+	<td contenteditable='true' id="loose:2"></td>
+	<td contenteditable='true' id="phase:2"></td>
+  </tr><tr>
+    <td contenteditable='true' id="sr_no:3"></td>
+	<td contenteditable='true' id="activity:3"></td>
+	<td contenteditable='true' id="earn:3"></td>
+	<td contenteditable='true' id="loose:3"></td>
+	<td contenteditable='true' id="phase:3"></td>
+  </tr><tr>
+    <td contenteditable='true' id="sr_no:4"></td>
+	<td contenteditable='true' id="activity:4"></td>
+	<td contenteditable='true' id="earn:4"></td>
+	<td contenteditable='true' id="loose:4"></td>
+	<td contenteditable='true' id="phase:4"></td>
+  </tr>
+  
+</table>
